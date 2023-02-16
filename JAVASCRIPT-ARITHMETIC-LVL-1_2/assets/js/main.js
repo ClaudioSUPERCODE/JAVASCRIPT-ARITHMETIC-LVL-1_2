50b52faa2ad83127d663a32d9e351444a4289cf8
ClaudioSUPERCODE <123948180+ClaudioSUPERCODE@users.noreply.github.com>
2023-02-16 19:04:43 +0100

    Add files via upload

---
+++ JAVASCRIPT-ARITHMETIC-LVL-1_2/assets/js/main.js
@@ -0,0 +1,18 @@
+let score = 5+5*10;
+console.log("Ergebnis "+score);
+
+score = (5+5)*10;
+console.log(score);
+
+score +=10;
+console.log (score);
+
+let zahl =1;
+zahl += 1;
+console.log (zahl);
+
+zahl++;
+console.log (zahl);
+
+zahl--;
+console.log (zahl)
